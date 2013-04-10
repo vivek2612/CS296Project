8118248dba052fe44c0426ba52c0ea8ffd379958
Yeshwanth <yeshwanth@yeshwanth-PC.(none)>
2013-04-10 19:58:50 +0530

    Final Commit before Name Change

--- scripts/helper5.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-rm -rf Temp5.csv
-while IFS=, read col1 col2 col3 col4 col5
-do
-    if [ "$col2" = " 42" ];
-    then
-    	echo $col3 >> ../data/Temp5.csv
-    fi	
-done < "../data/lab05_g14_data.csv"
